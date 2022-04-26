439bfb8afae2c81c21b98480f81c984262eb9ebf
InDEF <zzangka1122@gmail.com>
2022-04-26 22:19:25 +0900

    인텐트

---
+++ 201822267Intent/app/src/main/java/com/example/a201822267intent/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.a201822267intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:/"));
+                startActivity(intent);
+            }
+        });
+        Button button1 = (Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
